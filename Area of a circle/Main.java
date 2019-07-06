b667ad27a6c7e547125b001b8f293cb79b0206b8
praveen-bdm <52093231+praveen-bdm@users.noreply.github.com>
2019-07-06 11:59:49 +0530

    Test code pushed to repository.

---
+++ Area of a circle/Main.java
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float area;
+ float d;
+  scanf("%f",&d);
+ float r=d/2;
+  area=3.14*r*r;
+  printf("%.2f",area);
+  return 0;
+}
---
+++ Circumference of a circle/Main.java
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() 
+{ 
+  float r,crc;
+  scanf("%f",&r);
+  crc=2*3.14*r;
+  printf("%.2f",crc);
+  return 0;
+  
+ 	
+}
